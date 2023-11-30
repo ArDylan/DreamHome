07625b981050392c6fee7f6e7fb87934bee09e91
M-adhitya235 <126459544+M-adhitya235@users.noreply.github.com>
2023-11-30 09:59:19 +0800

    Delete DeswebTubes directory

--- DeswebTubes/src/components/button.jsx
+++
@@ -1,8 +0,0 @@
-// Komponen MyButton adalah contoh komponen fungsional yang menerima prop 'onClick'.
-export function Button({ onClick }) {
-  return (
-    <button onClick={onClick} className="border-4 p-4">
-      Tambah
-    </button>
-  );
-}
